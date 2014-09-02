15081abebc7978793c29405a7a13bd8cb96eca0f
pablolxdxs <pablolxdxs@gmail.com>
2014-09-01 23:43:16 -0500

    primer ejerci

---
+++ ejercio primero.js
@@ -0,0 +1,35 @@
+var cont=0;
+var b=0;
+var r; 
+var i=0;
+for(i=0;i<a.length;i++){  
+
+if( a[i]%i==0)
+              {
+
+              	cont=cont+1;
+                     
+                 }
+
+                if(cont > 2){
+                           
+                          }else{
+
+
+                          	alert( r +" es primo")
+                          }
+
+if(a[i]%2==0){
+		           b=+1
+		           b=a[i]
+		           r=a[i]
+		           
+		       alert(r +" es par") ;
+            }else{
+            alert( a[i]+"no es par");
+          }
+
+
+
+         }             
+                    
